d921ff16c5c1bb377302a0f7f3eec75b704d5c13
igoroliveira169 <igorgabriel2805@gmail.com>
2024-10-03 21:55:06 -0300

    first commit

---
+++ src/main/java/com/curso/suporteos/SuporteosApplication.java
@@ -0,0 +1,21 @@
+package com.curso.suporteos;
+
+import jakarta.persistence.Entity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@ComponentScan(basePackages = "com.curso")
+@EntityScan(basePackages = {"com.curso.domains","com.curso.domains.enums"})
+@EnableJpaRepositories(basePackages = "com.curso.repositories")
+@SpringBootApplication
+public class SuporteosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuporteosApplication.class, args);
+	}
+
+}
